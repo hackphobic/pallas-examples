3aefa20404e903533ae56ca4c39fb82a2c803a7e
Ziyad Alagla <ziyadalagla@gmail.com>
2025-01-26 14:05:38 +0300

    fixed a small bug

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,48 @@
 pub mod crypto;
 use std::hash::Hash;
+use std::fs;
+use std::io::{Write, Read};
 use blake2::{Blake2b, Blake2bVar};
 use pallas::ledger::{addresses, traverse::ComputeHash};
 use pallas::crypto::key::ed25519::{self, *};
 use pallas::crypto::hash::Hasher;
 
 use crypto::{
-    //address::{xprv_from_phrase}, 
+    //address::{xprv_from_phrase},
     keypair::{xprv_from_rng}
 };
 
 fn main() {
-    
-    let xprv: SecretKeyExtended = xprv_from_rng().expect("Error");
-    let leaked: [u8; SecretKeyExtended::SIZE] = unsafe { SecretKeyExtended::leak_into_bytes(xprv) };
-    //let pubkey_hash = xprv.public_key().compute_hash();
-    let mut hasher = Hasher::<224>::new();
-    //hasher.input(xprv.public_key());
-    //let payment_key = ShelleyPaymentPart::Key(Hash::<28>::from(xprv.public_key().compute_hash()));
-    print!("{:?}", leaked); //xprv.public_key().hash(&mut hasher));
-
-    let utf8 = leaked.iter().map(|&c| c as char).collect::<String>();
-    print!("{:?}", utf8);
+    if let Ok(key) = fs::read("C:\\Users\\is_st\\key.txt"){
+        let parsed_key: [u8; 64] = key[..64].try_into().expect("error");
+        let xprv: SecretKeyExtended = SecretKeyExtended::from_bytes(parsed_key).expect("Error");
+        let leaked: [u8; SecretKeyExtended::SIZE] = unsafe { SecretKeyExtended::leak_into_bytes(xprv) };
+        //let pubkey_hash = xprv.public_key().compute_hash();
+        let mut hasher = Hasher::<224>::new();
+        //hasher.input(xprv.public_key());
+        //let payment_key = ShelleyPaymentPart::Key(Hash::<28>::from(xprv.public_key().compute_hash()));
+        println!("Raw bytes: {:?}", leaked); //xprv.public_key().hash(&mut hasher));
+        //let utf8 = String::from_utf8(leaked.to_vec()).unwrap();
+        //let hex = leaked.iter().map(|byte| format!("{:02x}", byte)).collect::<String>();
+        println!();
+        println!("Hex encoded key: {:02X?}", leaked);
+        //let utf8 = leaked.iter().map(|&byte| String::from_utf8(byte)).collect::<String>();
+        //print!("{:?}", hex);
+    } else {
+        let xprv: SecretKeyExtended = xprv_from_rng().expect("Error");
+        let leaked: [u8; SecretKeyExtended::SIZE] = unsafe { SecretKeyExtended::leak_into_bytes(xprv)};
+        let write = fs::write("C:\\Users\\is_st\\key.txt", leaked);
+        //let pubkey_hash = xprv.public_key().compute_hash();
+        let mut hasher = Hasher::<224>::new();
+        //hasher.input(xprv.public_key());
+        //let payment_key = ShelleyPaymentPart::Key(Hash::<28>::from(xprv.public_key().compute_hash()));
+        println!("Raw bytes: {:?}", leaked); //xprv.public_key().hash(&mut hasher)).;
+        //let utf8 = String::from_utf8(leaked.to_vec()).unwrap();
+        //let hex = leaked.iter().map(|byte| format!("{:02x}", byte)).collect::<String>();
+        println!();
+        println!("Hex encoded key: {:02X?}", leaked);
+        //let utf8 = leaked.iter().map(|&byte| String::from_utf8(byte)).collect::<String>();
+        //print!("{:?}", hex);
+        println!("writing key to file: {:?}", write);
+    }
 }
