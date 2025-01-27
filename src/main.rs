2147eb51310e67655070734e59753c4914ebaaec
Ziyad Alagla <ziyadalagla@gmail.com>
2025-01-27 19:40:36 +0300

    bech32 fix

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-pub mod crypto;
+pub mod wallet;
 use std::hash::Hash;
 use std::fs;
 use std::io::{Write, Read};
 use blake2::{Blake2b, Blake2bVar};
-use pallas::ledger::{addresses, traverse::ComputeHash};
+use pallas::ledger::{
+    addresses::{ShelleyPaymentPart, ShelleyDelegationPart, Network, ShelleyAddress}, 
+    traverse::ComputeHash
+};
 use pallas::crypto::key::ed25519::{self, *};
 use pallas::crypto::hash::Hasher;
 
-use crypto::{
+use wallet::{
     //address::{xprv_from_phrase},
     keypair::{xprv_from_rng}
 };
@@ -16,15 +19,21 @@ fn main() {
     if let Ok(key) = fs::read("C:\\Users\\is_st\\key.txt"){
         let parsed_key: [u8; 64] = key[..64].try_into().expect("error");
         let xprv: SecretKeyExtended = SecretKeyExtended::from_bytes(parsed_key).expect("Error");
-        let leaked: [u8; SecretKeyExtended::SIZE] = unsafe { SecretKeyExtended::leak_into_bytes(xprv) };
-        //let pubkey_hash = xprv.public_key().compute_hash();
-        let mut hasher = Hasher::<224>::new();
+        let pubkey_hash = xprv.public_key().compute_hash();
+        let leaked: [u8; SecretKeyExtended::SIZE] = unsafe { SecretKeyExtended::leak_into_bytes(xprv)};
         //hasher.input(xprv.public_key());
-        //let payment_key = ShelleyPaymentPart::Key(Hash::<28>::from(xprv.public_key().compute_hash()));
+        let payment_part = ShelleyPaymentPart::Key(pubkey_hash);
+        let delegation_part = ShelleyDelegationPart::Null;
+        let network_part = Network::Testnet;
+        let address = ShelleyAddress::new(network_part, payment_part, delegation_part).to_bech32().expect("Error: Bech32");
+        
+        println!("Address: {:?}", address);
+        println!("---------------------------------");
+        println!();
         println!("Raw bytes: {:?}", leaked); //xprv.public_key().hash(&mut hasher));
         //let utf8 = String::from_utf8(leaked.to_vec()).unwrap();
         //let hex = leaked.iter().map(|byte| format!("{:02x}", byte)).collect::<String>();
-        println!();
+        println!("---------------------------------");
         println!("Hex encoded key: {:02X?}", leaked);
         //let utf8 = leaked.iter().map(|&byte| String::from_utf8(byte)).collect::<String>();
         //print!("{:?}", hex);
